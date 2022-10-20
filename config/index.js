d09631e82123d7bd3e73fab2744e89704a6ec961
atif11802 <ratul0947@gmail.com>
2022-10-20 20:16:12 +0600

    config changed

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
 	? "http://localhost:3000"
-	: "https://nextjs-ecommerce-4e8y.vercel.app/";
+	: "https://nextjs-ecommerce-xi-eight.vercel.app/";
